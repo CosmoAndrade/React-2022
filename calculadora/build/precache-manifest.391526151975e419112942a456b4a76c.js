9755cf23f2092146ee84686953dffda5a2abac1e
COSMO ANDRADE FELIX <57240412+CosmoAndrade@users.noreply.github.com>
2022-01-10 20:22:04 -0300

    Add files via upload

---
+++ calculadora/build/precache-manifest.391526151975e419112942a456b4a76c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e4ff4f396bbec6be23d4a0dabe5b976",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8f6a58f83ff2e5523617",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "c9ad00e2e7c15e40db1f",
+    "url": "/static/css/main.d85e6858.chunk.css"
+  },
+  {
+    "revision": "8f6a58f83ff2e5523617",
+    "url": "/static/js/2.952bfac9.chunk.js"
+  },
+  {
+    "revision": "c9ad00e2e7c15e40db1f",
+    "url": "/static/js/main.a46a8a13.chunk.js"
+  },
+  {
+    "revision": "c35faa878882107427df",
+    "url": "/static/js/runtime-main.69668034.js"
+  }
+]);
